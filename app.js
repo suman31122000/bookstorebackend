ae5cef513a1172860a126b7ffaaefad782ab3ba0
suman31122000 <sumanbando31122000@gmail.com>
2024-05-01 20:48:30 +0530

    hii

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors=require('cors');
-
+const PORT=process.env.PORT || 3000;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
